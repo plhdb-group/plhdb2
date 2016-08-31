d1f4eea206661f83906e432011de178ae8c65a06
Karl O. Pinc <kop@meme.com>
2016-08-31 15:46:04 -0400

    Get rid of "permanent" checkbox

--- adminer/production/index.php
+++ adminer/production/index.php
@@ -7,6 +7,7 @@ function adminer_object() {
       global $drivers;
       ?>
       <input type="hidden" name="auth[driver]" value="pgsql" >
+      <input type="hidden" name="auth[permanent]" value="1" >
 <table cellspacing="0">
    <tr><th><?php echo lang('Server'); ?><td><input name="auth[server]" value="<?php echo h(SERVER); ?>" title="hostname[:port]" placeholder="localhost" autocapitalize="off">
    <tr><th><?php echo lang('Username'); ?><td><input name="auth[username]" id="username" value="<?php echo h($_GET["username"]); ?>" autocapitalize="off">
@@ -18,7 +19,6 @@ function adminer_object() {
 </script>
 <?php
     echo "<p><input type='submit' value='" . lang('Login') . "'>\n";
-    echo checkbox("auth[permanent]", 1, $_COOKIE["adminer_permanent"], lang('Permanent login')) . "\n";
     }
 
     function name() {
