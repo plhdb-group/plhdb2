897bd84b45437e3f1d71e1a88f2b9156b6660fbe
Karl O. Pinc <kop@meme.com>
2016-08-31 15:18:20 -0400

    Make "plhdb" the default db

--- adminer/production/index.php
+++ adminer/production/index.php
@@ -11,7 +11,7 @@ function adminer_object() {
    <tr><th><?php echo lang('Server'); ?><td><input name="auth[server]" value="<?php echo h(SERVER); ?>" title="hostname[:port]" placeholder="localhost" autocapitalize="off">
    <tr><th><?php echo lang('Username'); ?><td><input name="auth[username]" id="username" value="<?php echo h($_GET["username"]); ?>" autocapitalize="off">
    <tr><th><?php echo lang('Password'); ?><td><input type="password" name="auth[password]">
-   <tr><th><?php echo lang('Database'); ?><td><input name="auth[db]" value="<?php echo h($_GET["db"]); ?>" autocapitalize="off">
+   <tr><th><?php echo lang('Database'); ?><td><input name="auth[db]" value="<?php echo h($_GET["db"])==''?'plhdb':h($_GET["db"]); ?>" placeholder="plhdb" autocapitalize="off">
 </table>
 <script type="text/javascript">
  focus(document.getElementById('username'));
